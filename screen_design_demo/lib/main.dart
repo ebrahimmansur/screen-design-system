e8b2c8636f171fe4b21032327f4a0322d8650d51
Ebrahim Mansur <ebrahimmansur90117@gmail.com>
2023-01-19 20:25:48 +0200

    ✅ add: design system barrel

--- screen_design_demo/lib/main.dart
+++ screen_design_demo/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'screen_design/screen_design.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,21 +10,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return ScreenDesignInheritedWidget.build(
+      child: MaterialApp(
+        title: 'Screen Design Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: const ScreenDesignLayout(
+          tablet: MyHomePage(title: 'Screen Design - Tablet'),
+          mobile: MyHomePage(title: 'Screen Design - Mobile'),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
--- screen_design_demo/lib/screen_design/screen_design.dart
+++ screen_design_demo/lib/screen_design/screen_design.dart
@@ -3,3 +3,4 @@ export 'screen_deisgn_inherited_widget.dart';
 export 'screen_design_data.dart';
 export 'package:equatable/equatable.dart';
 export 'package:flutter/material.dart';
+export 'screen_deign_layout.dart';
