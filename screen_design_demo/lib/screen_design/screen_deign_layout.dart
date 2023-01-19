91d8957d0abb3bf2fadc1005552d314239d548a2
Ebrahim Mansur <ebrahimmansur90117@gmail.com>
2023-01-19 20:23:42 +0200

    ✅ add: screen design layout widget

---
+++ screen_design_demo/lib/screen_design/screen_deign_layout.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screen_design_extension.dart';
+
+///The widget will resolve the screen layout process based on the screen information
+///coming from the design screen data.
+///
+///author - Ebrahim Mansur.
+class ScreenDesignLayout extends StatelessWidget {
+  final Widget mobile;
+  final Widget tablet;
+  const ScreenDesignLayout({
+    Key? key,
+    required this.mobile,
+    required this.tablet,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return context.screenData.isTablet ? tablet : mobile;
+  }
+}
