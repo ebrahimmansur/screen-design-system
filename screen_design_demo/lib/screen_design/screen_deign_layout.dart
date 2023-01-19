253f8eac8349a490821e0ad20a2b9439876764ee
Ebrahim Mansur <ebrahimmansur90117@gmail.com>
2023-01-19 20:38:31 +0200

    ♻️ update : doc

--- screen_design_demo/lib/screen_design/screen_deign_layout.dart
+++ screen_design_demo/lib/screen_design/screen_deign_layout.dart
@@ -4,6 +4,7 @@ import 'screen_design_extension.dart';
 ///The widget will resolve the screen layout process based on the screen information
 ///coming from the design screen data.
 ///
+///
 ///author - Ebrahim Mansur.
 class ScreenDesignLayout extends StatelessWidget {
   final Widget mobile;
