81c19863db58b3e560e644db208ca57a05da9f00
Ebrahim Mansur <ebrahimmansur90117@gmail.com>
2023-01-19 20:24:21 +0200

    ✅ add: screen design barral

---
+++ screen_design_demo/lib/screen_design/screen_design.dart
@@ -0,0 +1,5 @@
+export 'screen_design_extension.dart';
+export 'screen_deisgn_inherited_widget.dart';
+export 'screen_design_data.dart';
+export 'package:equatable/equatable.dart';
+export 'package:flutter/material.dart';
